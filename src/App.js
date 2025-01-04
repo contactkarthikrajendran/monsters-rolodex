ca9f0bc539b710f46da1a975327a8deda359d2f0
contactkarthikrajendran <110722371+contactkarthikrajendran@users.noreply.github.com>
2025-01-04 22:59:03 +0530

    <Karthik> Successful API Fetch

--- src/App.js
+++ src/App.js
@@ -9,39 +9,35 @@ class App extends Component{
     super();
 
     this.state = {
-
-      monsters:[
-        {
-          id: '1',
-          name:'Linda'
-        },
-        {
-          id: '2',
-          name:'Meenu'
-        },
-        {
-          id: '3',
-          name:'Karthik'
-        },
-        {
-          id: '4',
-          name:'test'
-        }
-      ]
-      
+      monsters:[],    
     };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response)=> response.json())
+    .then((users)=> this.setState(
+        ()=>{
+         return {monsters: users}
+        },
+        ()=>{
+          console.log(this.state.monsters);
+        }
+      ));
   }
 
 
   render(){
     return (
            <div className="App">
-            {this.state.monsters.map((a)=>{
-             return <div id ={a.id}>              
-              <h1>{a.name}</h1>             
-             </div>            
+            {this.state.monsters.map((monster)=>{
+             return (
+              <div key ={monster.id}>              
+                <h1>{monster.name}</h1>             
+             </div> );          
             })}
            </div>
+
          );
   }
 }
